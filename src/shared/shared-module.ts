4b03705b294ae1bbfc502966e2201029c4ac4e6c
aash100 <baranwalaashish@gmail.com>
2025-09-08 20:16:34 +0530

    New form and module changes

--- src/shared/shared-module.ts
+++ src/shared/shared-module.ts
@@ -1,12 +1,11 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MyForm } from './components/my-form/my-form';
 
 @NgModule({
-  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [MyForm],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  exports: [MyForm],
+  // exports: [MyForm],
 })
 export class SharedModule {}
